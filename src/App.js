fbf2ae83a118fc1dcb0d6910ee2a10b82130a10b
zhangningle <zhangningle@58.com>
2022-11-22 14:53:09 +0800

    feat: web 截图

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { html2canvasHandle, domToImageHandle, WebRTCHandle } from './utils.js'
 function App() {
   return (
-    <div className="App">
+    <div className="App" id="root">
+      <video style={{ width: '100vw', height: '100vh', position: 'absolute', top: '0', left: '0' }} id="video"></video>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img id="perview" crossOrigin="anonymous" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K" />
         <a
           className="App-link"
           href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
         >
-          Learn React
+          学习截图方案
         </a>
+        <button onClick={() => html2canvasHandle('root')}>html2canvas点击截图</button>
+        <button onClick={() => domToImageHandle('root')}>dom-to-tmage点击截图</button>
+        <button onClick={() => WebRTCHandle('video')}>web RTC点击截图</button>
       </header>
     </div>
   );
---
+++ src/utils.js
@@ -0,0 +1,88 @@
+
+import html2canvas from 'html2canvas'
+import domtoimage from 'dom-to-image-more'
+
+export const domToImageHandle = (eleId) => {
+  domtoimage
+    .toPng(document.getElementById(eleId))
+    .then(function (dataUrl) {
+      let a = document.createElement('a')
+      a.href = dataUrl
+      a.download = 'test'
+      a.click()
+    })
+    .catch(function (error) {
+      console.error('生成失败', error)
+    })
+}
+
+export const html2canvasHandle = (eleId) => {
+  html2canvas(document.getElementById(eleId), {
+    //superMap整个页面的节点
+    backgroundColor: null, //画出来的图片有白色的边框,不要可设置背景为透明色（null）
+    allowTaint: true,
+    useCORS: true, //支持图片跨域
+    scale: 1, //设置放大的倍数
+  })
+    .then((canvas) => {
+      console.log('html2canvas success')
+      //截图用img元素承装，显示在页面的上
+      const imgData = canvas.toDataURL('image/png') // toDataURL :图片格式转成 base64
+      //回传子组件base64
+      //  cab(this.imgUrl)
+      //如果你需要下载截图，可以使用a标签进行下载
+      let a = document.createElement('a')
+      a.href = imgData
+      a.download = 'test'
+      a.click()
+    })
+    .catch((err) => {
+      console.log('html2canvas err', err)
+    })
+}
+
+
+
+export function WebRTCHandle(videoId) {
+  const videoElem = document.getElementById(videoId)
+  // videoElem.style.width = window.screen.width + 'px'
+  // videoElem.style.height = window.screen.height + 'px'
+
+  const displayMediaOptions = {
+    audio: false,
+    video: { width: window.screen.width, height: window.screen.height }, // cursor: "always"
+  }
+
+  navigator.mediaDevices
+    .getDisplayMedia(displayMediaOptions)
+    .then((stream) => {
+      videoElem.srcObject = stream
+      setTimeout(() => {
+        var canvas = document.createElement('canvas')
+        canvas.width = videoElem.clientWidth
+        canvas.height = videoElem.clientHeight
+        canvas
+          .getContext('2d')
+          .drawImage(
+            videoElem,
+            0,
+            0,
+            window.screen.width,
+            window.screen.height,
+            0,
+            0,
+            canvas.width,
+            canvas.height
+          )
+        var dataURL = canvas.toDataURL('image/webp')
+        let a = document.createElement('a')
+        a.href = dataURL
+        a.download = 'test'
+        a.click()
+        let tracks = videoElem.srcObject.getTracks()
+        tracks.forEach((track) => track.stop())
+        videoElem.srcObject = null
+      }, 200)
+    })
+    .finally(() => { })
+}
